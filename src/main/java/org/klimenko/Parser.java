345315eb4f6cccb2352e57b3a794d725b416674d
H1EBUWEK <96592925+H1EBUWEK@users.noreply.github.com>
2023-06-22 01:53:24 -0700

    Changed Exception Whrong amount of arguments

--- src/main/java/org/klimenko/Parser.java
+++ src/main/java/org/klimenko/Parser.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Parser {
     public static Map<String, Object> ParsingMoney(String[] strings, String command) throws Exception {
         if(strings.length<2){
-            throw new Exception("Wrong amount of arguments. Usage:\n"+
+            throw new Exception("Not enough arguments. Usage:\n"+
                     command + " @<id> <amount>");
         }
         HashMap<String, Object> map = new HashMap<>();
